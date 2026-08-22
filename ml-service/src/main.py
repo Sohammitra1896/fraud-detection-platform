2f6fbb6dfcbbe17cbe8cd8fedfcd98a1bf086ee7
Sandip Mitra <sandipmitra@Sandips-MacBook-Air.local>
2026-08-22 12:08:02 +0530

    Allow deployed frontend origin

--- ml-service/src/main.py
+++ ml-service/src/main.py
@@ -58,9 +58,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://fraudlens-frontend-0h16.onrender.com",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
