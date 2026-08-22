8fcd6909f245690f694ece20dbfcaa5496c9d693
Sandip Mitra <sandipmitra@Sandips-MacBook-Air.local>
2026-08-22 11:20:27 +0530

    Prepare backend for deployment

--- ml-service/src/database.py
+++ ml-service/src/database.py
@@ -1,12 +1,20 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = (
-    "postgresql+psycopg://"
-    "sandipmitra@/fraudlens"
-)
+load_dotenv()
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError(
+        "DATABASE_URL environment variable is not set."
+    )
 
 
 engine = create_engine(
